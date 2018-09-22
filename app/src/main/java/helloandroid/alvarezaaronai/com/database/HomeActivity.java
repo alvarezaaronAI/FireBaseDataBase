0a9dd6b1f85f5eddc7a51f42fd2450d3ec17e473
alvarezaaronai <alvarezaaron80@gmail.com>
2018-09-21 19:55:16 -0700

    added realTime database data on click listners

--- app/src/main/java/helloandroid/alvarezaaronai/com/database/HomeActivity.java
+++ app/src/main/java/helloandroid/alvarezaaronai/com/database/HomeActivity.java
@@ -1,13 +1,64 @@
 package helloandroid.alvarezaaronai.com.database;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+    //Controller for Member Variables
+    TextView mConditionTextView;
+    Button mButtonSunny;
+    Button mButtonFoggy;
+
+    //Connect to DataBase
+    DatabaseReference myRoofRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = myRoofRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mConditionTextView = (TextView) findViewById(R.id.textViewCondition);
+        mButtonSunny = (Button) findViewById(R.id.buttonSunny);
+        mButtonFoggy = (Button) findViewById(R.id.buttonFoggy);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mConditionTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        mButtonSunny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("Sunny!");
+            }
+        });
+        mButtonFoggy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("Foggy!");
+            }
+        });
     }
 }
